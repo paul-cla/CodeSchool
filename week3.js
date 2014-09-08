bf4bfa9483c1cecfd17a3ec57f0dea0e84f1558a
Paul Clarkin <paulclarkin@yahoo.com>
2014-09-08 21:38:44 +0100

    Week 3

---
+++ week3.js
@@ -0,0 +1,26 @@
+var submitButton = document.getElementById('submit');
+
+submitButton.onclick = function(){
+
+	var name = document.getElementById('name').value;
+
+	var colour = document.getElementById('colour').value;
+
+	var animal = "other";
+
+	if(document.getElementById('cats').checked){
+		animal = "cat";
+	} else if(document.getElementById('dogs').checked) {
+		animal = "dog"
+	}
+
+	showResults(name, colour, animal);
+}
+
+function showResults(name, colour, animal){
+	document.getElementById('name-result').innerText = name;
+
+	document.getElementById('results').style.display = "block";
+	document.getElementById(animal).style.display = "block";
+	document.getElementById('results').style.background = colour;
+}
